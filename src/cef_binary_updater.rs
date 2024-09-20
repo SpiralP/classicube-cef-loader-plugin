cfd42bba6a7bbcd3e77ab6850d1b7f8d8cc0370f
SpiralP <2898730+SpiralP@users.noreply.github.com>
2024-09-20 19:16:43 +0000

    Bump cef_binary to `128.4.12+g1d7a1f9+chromium-128.0.6613.138`

--- src/cef_binary_updater.rs
+++ src/cef_binary_updater.rs
@@ -25,7 +25,7 @@ use crate::{async_manager, print_async, status};
 #[cfg(not(all(target_os = "linux", target_arch = "x86")))]
 macro_rules! cef_version {
     () => {
-        "128.4.9+g9840ad9+chromium-128.0.6613.120"
+        "128.4.12+g1d7a1f9+chromium-128.0.6613.138"
     };
 }
 
