04cd570d9f237b5e282dca33652693c081d4519d
SpiralP <spiral.palmer@gmail.com>
2024-12-26 14:21:16 -0800

    nix: use name/version from manifest and commit hash in version; use allowBuiltinFetchGit so we don't have to manage hashes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
     let
       inherit (nixpkgs) lib;
 
@@ -17,7 +17,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = rustManifest.package.name;
-            version = rustManifest.package.version;
+            version = "${rustManifest.package.version}-${self.shortRev or self.dirtyShortRev}";
 
             src = lib.sourceByRegex ./. [
               "^\.cargo(/.*)?$"
@@ -28,10 +28,7 @@
 
             cargoLock = {
               lockFile = ./Cargo.lock;
-              outputHashes = {
-                "async-dispatcher-0.1.0" = "sha256-GHKvmhHXvjSI2DZj+rdJugYcrNDkw8SGxRcNzGsF0VM=";
-                "classicube-helpers-3.0.0+classicube.1.3.7" = "sha256-3hWKS6NmAH0x+SOi/nBKJLIQi/3ilG7WSRrPvF++wGE=";
-              };
+              allowBuiltinFetchGit = true;
             };
 
             nativeBuildInputs = with pkgs; [
